9f8305045051d6db23377225d3639521ee0e8887
burt <skyfe79@gmail.com>
2016-03-21 00:03:40 +0900

    commit How to use XCPlaygroundPage.liveview

---
+++ about.XCPlayground/01_XCPlaygroundPage_LiveView.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import XCPlayground
+
+//: UIView가 실시간으로 렌더링 되는 것을 볼 수 있다.
+
+
+let someView = UIView(frame: CGRectMake(0, 0, 100, 100))
+someView.backgroundColor = UIColor.redColor()
+
+//: Timeline Assistant를 켜야 한다.
+XCPlaygroundPage.currentPage.liveView = someView
