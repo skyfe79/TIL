5752f1d9b0fb2b4f3d9505cf61e59de0323fa5d7
burt <sungcheol.kim@hpcnt.com>
2016-03-31 17:01:52 +0900

    commit somethings

---
+++ about.iOS/about.GPUImage/CustomOutput/CustomOutput/GPUImageTempFilter.h
@@ -0,0 +1,13 @@
+//
+//  GPUImageTempFilter.h
+//  CustomOutput
+//
+//  Created by burt on 3/31/16.
+//  Copyright © 2016 burt. All rights reserved.
+//
+
+#import "GPUImageFilter.h"
+
+@interface GPUImageTempFilter : GPUImageFilter
+
+@end
---
+++ about.iOS/about.GPUImage/CustomOutput/CustomOutput/GPUImageTempFilter.m
@@ -0,0 +1,13 @@
+//
+//  GPUImageTempFilter.m
+//  CustomOutput
+//
+//  Created by burt on 3/31/16.
+//  Copyright © 2016 burt. All rights reserved.
+//
+
+#import "GPUImageTempFilter.h"
+
+@implementation GPUImageTempFilter
+
+@end
