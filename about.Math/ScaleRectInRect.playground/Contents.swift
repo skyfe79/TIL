22d459a4b4704c91e6370ec09424e45c41f7012a
burt <sungcheol.kim@hpcnt.com>
2016-04-04 11:54:18 +0900

    modify CustomOutput example

--- about.Math/ScaleRectInRect.playground/Contents.swift
+++ about.Math/ScaleRectInRect.playground/Contents.swift
@@ -49,7 +49,7 @@ func ScaleToAspectFitRectInRect(rfit : CGRect, _ rtarget : CGRect) -> CGFloat
     return CGRectGetHeight(rtarget) / CGRectGetHeight(rfit);
 }
 
-func AspectFitRectInRect(rfit : CGRect, rtarget : CGRect) -> CGRect
+func AspectFitRectInRect(rfit : CGRect, _ rtarget : CGRect) -> CGRect
 {
     let s = ScaleToAspectFitRectInRect(rfit, rtarget);
     let w = CGRectGetWidth(rfit) * s;
@@ -75,6 +75,18 @@ func AspectFitRectAroundRect(rfit : CGRect, _ rtarget : CGRect) -> CGRect
     return CGRectMake(x, y, w, h);
 }
 
-
+AspectFitRectInRect(CGRectMake(100, 100, 370, 155), CGRectMake(200, 200, 300, 375))
 AspectFitRectAroundRect(CGRectMake(100, 100, 370, 155), CGRectMake(200, 200, 300, 375))
+
+
+let rect = CGRectMake(0, 0, 200, 100)
+var roll : CGFloat = 180
+CGRectApplyAffineTransform(rect, CGAffineTransformMakeRotation(fmod((90 + roll), 360) * CGFloat(M_PI)/180.0))
+roll = 270
+CGRectApplyAffineTransform(rect, CGAffineTransformMakeRotation(fmod((90 + roll), 360) * CGFloat(M_PI)/180.0))
+
+let rect180 = CGRectMake(0.670694335616827, 0.5111582027228, 0.0244140625, 0.0244140625)
+let rect270 = CGRectMake(0.623245116843645, 0.615251952810286, 0.01220703125, 0.048828125)
+
+
 
